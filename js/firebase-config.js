7a030808de897a9de1ebb3c547954630fd59d35e
servicehook <kamarjahankammu@gmail.com>
2025-09-26 22:33:53 +0530

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+// IMPORTANT: Get this from your Firebase project settings
+// Go to Project Settings -> General -> Your apps -> Firebase SDK snippet -> Config
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
